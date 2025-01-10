333808e91ea34388bad1a2d7c365672cc2e2ad31
Benjamin <146848539+Benjacrack55@users.noreply.github.com>
2025-01-10 18:40:50 -0300

    Create 16.c

---
+++ ejercicios-basicos/16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Numero: ");
+    scanf("%d", &num);
+    int pares = 0;
+    int impares = 0;
+    int mayor = num % 10;
+    int menor = num % 10;
+    int n = num;
+
+    while (n > 0) {
+        if ((n % 10) % 2 == 0) {
+            pares++;
+        } else {
+            impares++;
+        }
+        if (n % 10 > mayor) {
+            mayor = n % 10;
+        } else if (n % 10 < menor) {
+            menor = n % 10;
+        }
+    }
+
+    printf("Cantidad de pares: %d, cantidad de impares: %d, mayor numero: %d, menor numero: %d \n", pares, impares, mayor, menor);
+    
+    return 0;
+}
