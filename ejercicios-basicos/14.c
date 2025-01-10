a8f637a64776fec9eef731884a0913ac7111fb84
Benjamin <146848539+Benjacrack55@users.noreply.github.com>
2025-01-10 18:14:09 -0300

    Create 14.c

---
+++ ejercicios-basicos/14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    for (int i = 1; i <= 10; i++) {
+        printf("%d: ", i);
+        int j = 1;
+        while (i * j <= 100) {
+            printf("%d ", i*j);
+            j++;
+        } printf("\n");
+    } printf("\n");
+    
+    return 0;
+}
