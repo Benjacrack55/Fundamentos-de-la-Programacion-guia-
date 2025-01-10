7c7305e21ac1df2cd7db34ecdb5021f41072ff04
Benjamin <146848539+Benjacrack55@users.noreply.github.com>
2025-01-10 18:47:04 -0300

    Create 17.c

--- ejercicios-basicos/17.c
+++ ejercicios-basicos/17.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    for (int i = 1; i <= 5 ; i++) {
+    int altura;
+    printf("Altura: ");
+    scanf("%d", &altura);
+
+    for (int i = 1; i <= altura ; i++) {
         for (int j = 1; j <= i; j++) {
             printf("*");
         } printf("\n");
